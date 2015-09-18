b2920741e7713cc7afb857b557cea0dd09ed0b24
Vadim Mazur <mazurva@gmail.com>
2015-09-18 20:39:02 +0600

    Update Travic-CI

---
+++ tests/unit/_config.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'id' => 'app-console',
+    'class' => 'yii\console\Application',
+    'basePath' => \Yii::getAlias('@tests'),
+    'runtimePath' => \Yii::getAlias('@tests/_output'),
+    'bootstrap' => [],
+    'components' => [
+        'user' => [
+            'class' => 'data\User',
+            //'identityClass' => 'data\User',
+            //'enableAutoLogin' => true,
+        ],
+        'db' => [
+            'class' => '\yii\db\Connection',
+            'dsn' => 'sqlite:'.\Yii::getAlias('@tests/_output/temp.db'),
+            'username' => '',
+            'password' => '',
+        ]
+    ]
+];
