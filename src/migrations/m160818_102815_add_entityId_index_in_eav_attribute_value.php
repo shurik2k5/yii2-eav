85267c0f21008cac49f5d78081568e9a16e50381
Duke <duke.nsk@gmail.com>
2016-08-18 17:45:10 +0700

    Add entityId index in table eav_attribute_value
    Добавлен индекс по полю entityId в таблицу eav_attribute_value для
    оптимизации выборки

---
+++ src/migrations/m160818_102815_add_entityId_index_in_eav_attribute_value.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Migration;
+
+
+class m160818_102815_add_entityId_index_in_eav_attribute_value extends Migration
+{
+    public $tableName = '{{%eav_attribute_value}}';
+
+    public function up()
+    {
+        $this->createIndex('idx_eav_attribute_value_entityId', $this->tableName, 'entityId');
+    }
+
+    public function down()
+    {
+        $this->dropIndex('idx_eav_attribute_value_entityId', $this->tableName);
+        return false;
+    }
+
+}
