12fed0f4a2e6839c6594f6079c630d0e71185840
Duke <duke.nsk@gmail.com>
2016-08-23 11:02:43 +0700

    Сorrecting spelling in Russian localization
    Исправлена орфография в русской локализации

--- src/messages/ru/eav.php
+++ src/messages/ru/eav.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'EAV Fields constructor' => 'Конструктор EAV атрибутов',
+    'EAV Fields constructor' => 'Конструктор EAV-атрибутов',
     'Descriptions' => 'Описания',
     'Route' => 'Маршрут',
     'Description' => 'Описание',
-    'Operations' => 'Дествия',
+    'Operations' => 'Действия',
     'Fields constructor' => 'Конструктор атрибутов',
     'Options constructor' => 'Конструктор параметров',
     'Types constructor' => 'Конструктор типов',
@@ -20,15 +20,15 @@ return [
     'Name' => 'Имя',
     'Label' => 'Метка',
     'Default Value' => 'Значение по умолчанию', 
-    'Default Option ID' => 'Идентификатор параметра поумолчанию',
-    'Required' => 'Обязатльный',
+    'Default Option ID' => 'Идентификатор параметра по умолчанию',
+    'Required' => 'Обязательный',
     'Order' => 'Порядок',
     'Description' => 'Описание',
     
     'Sort order' => 'Порядок сортировки',
     'Attribute ID' => 'Идентификатор атрибута',
     'Value' => 'Значение',
-    'Default option Id' => 'Идентификатор параметра поумолчанию',
+    'Default option Id' => 'Идентификатор параметра по умолчанию',
     
     'Attribute ID' => 'Идентификатор атрибута',
     'Rules' => 'Правила',
@@ -36,7 +36,7 @@ return [
     'Visible' => 'Видимый',
     'Attribute name' => 'Название атрибута',
     
-    'Handler Class' => 'Класс обработчик',
+    'Handler Class' => 'Класс-обработчик',
     'Store Type' => 'Тип хранилища',
     
     'Entity ID' => 'Идентификатор сущности',
@@ -47,23 +47,23 @@ return [
     'ID Category' => 'Идентификатор категории',
     'ID Model' => 'Идентификатор модели',
     
-    'Dynamic model data were no attributes.' => 'В модели нет EAV атрибутов',
-    'Dynamic model data were not save due to validation error.' => 'EAV атрибуты не были сохранены из-за ошибки валидации.',
+    'Dynamic model data were no attributes.' => 'В модели нет EAV-атрибутов',
+    'Dynamic model data were not save due to validation error.' => 'EAV-атрибуты не были сохранены из-за ошибки валидации.',
     'The description of field' => 'Описание атрибута',
    
     'The name of field' => 'Название атрибута',
     'The label of field' => 'Метка атрибута',
     'Entity name' => 'Название сущности',
-    'The default value of field' => 'Значение атрибута поумолчанию',
+    'The default value of field' => 'Значение атрибута по умолчанию',
     
     'Search' => 'Поиск',
     'Reset' => 'Очистить',
     
     'EAV' => 'EAV',
     'Fields' => 'Атрибуты',
-    'Create Eav Attribute' => 'Создание EAV атрибута',
-    'Update Eav Attribute' => 'Редактирование EAV атрибута',
-    'The type of entity' => 'Тип аттрибута',
+    'Create Eav Attribute' => 'Создание EAV-атрибута',
+    'Update Eav Attribute' => 'Редактирование EAV-атрибута',
+    'The type of entity' => 'Тип атрибута',
     
     
     'Are you sure you want to delete this item?' => 'Вы уверены, что хотите удалить этот элемент?',
@@ -72,23 +72,23 @@ return [
     'This is the ActiveRecord class associated with the table that EavEntity will be built upon.
                 You should provide a fully qualified class name, e.g., <code>app\models\Post</code>.' => 
     'Это класс ActiveRecord, связанный с таблицей, для которого создается сущность. Вы должны указать полное имя класса, например, <code>app\models\Post</code>.',
-    'This category id to bind to the directory. The default is not used and may be 0.' => 'Это идентификатор категории для объединения сущностей. По умолчанию не используется и может быть 0.',
+    'This category id to bind to the directory. The default is not used and may be 0.' => 'Это идентификатор категории для объединения сущностей. По умолчанию не используется и может быть равен 0.',
     
-    'Create Eav Entity' => 'Создание EAV сущности',
-    'Eav Entities' => 'EAV сущности',
-    'Update Eav Entity' => 'Редактирование EAV сущности',
+    'Create Eav Entity' => 'Создание EAV-сущности',
+    'Eav Entities' => 'EAV-сущности',
+    'Update Eav Entity' => 'Редактирование EAV-сущности',
     
-    'Create Eav Attribute Option' => 'Создание параметра EAV атрибута',
-    'Eav Attribute Options' => 'Параметры EAV атрибута',
-    'Update Eav Attribute Option' => 'Редактирование параметра EAV атрибута',
+    'Create Eav Attribute Option' => 'Создание параметра EAV-атрибута',
+    'Eav Attribute Options' => 'Параметры EAV-атрибута',
+    'Update Eav Attribute Option' => 'Редактирование параметра EAV-атрибута',
     
-    'Create Eav Attribute Type' => 'Создание типа EAV атрибута',
-    'Eav Attribute Types' => 'Типы EAV атрибутов',
-    'Update Eav Attribute Type' => 'Редактирование типа EAV атрибута',
+    'Create Eav Attribute Type' => 'Создание типа EAV-атрибута',
+    'Eav Attribute Types' => 'Типы EAV-атрибутов',
+    'Update Eav Attribute Type' => 'Редактирование типа EAV-атрибута',
     
-    'Create Eav Attribute Value' => 'Создание значения EAV атрибута',
-    'Eav Attribute Values' => 'Значения EAV атрибутов',
-    'Update Eav Attribute Value' => 'Режатирование значения EAV атрибута',
+    'Create Eav Attribute Value' => 'Создание значения EAV-атрибута',
+    'Eav Attribute Values' => 'Значения EAV-атрибутов',
+    'Update Eav Attribute Value' => 'Редактирование значения EAV-атрибута',
     
     'Attribute name must contain latin word characters only.' => 'Имя атрибута должно содержать только латинские буквы.',
     'Type name must contain latin word characters only.' => 'Имя атрибута должно содержать только латинские буквы.',
@@ -99,6 +99,6 @@ return [
     'Input numericfield' => 'Цифровое поле',
     'Radio' => 'Список с выбором',
     'Input textfield' => 'Текстовая строка',
-    'Input textarea' => 'Текствое поле',
+    'Input textarea' => 'Текстовое поле',
 
 ];
